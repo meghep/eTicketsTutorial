ffe1650b88bd67d3c3d8f8b4c335ff95cce5f355
Megan Hepner <megan.hepner@gmail.com>
2022-12-13 17:01:23 -0800

    End of Section 6, Models, Views, Controllers, DbContext

---
+++ Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTicketsTutorial.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies{ get; set; }
+    }
+}
